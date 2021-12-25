dd0adf54a25cf49666807a13ec663b03600d7464
reuttol <reut.tole@gmail.com>
2021-12-25 23:38:03 +0200

    Removed broken image

--- src/pets.js
+++ src/pets.js
@@ -1,7 +1,6 @@
 export const petsArray = [
     {name: "Mufasa", imageURL: "https://images.vectorhq.com/images/previews/f49/mufasa-psd-458659.png" },
     {name: "Heihei", imageURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLbQ6aO-_9vKEriYzS-nH6nTf_UEj6rHjjWg&usqp=CAU" },
-    {name: "Pascal", imageURL: "https://www.pngall.com/wp-content/uploads/2017/03/Pascal.png" },
     {name: "Abu", imageURL: "https://www.disneyclips.com/images/images/abu-jugglings.gif" },
     {name: "Burni", imageURL: "https://wallpaperaccess.com/full/2352954.jpg" },
     {name: "Sebastian", imageURL: "http://www.cartoonbucket.com/wp-content/uploads/2015/08/Sebastian-Cartoon-Picture.png" },
